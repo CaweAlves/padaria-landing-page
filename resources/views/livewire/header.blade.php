274187e8551cde20055dab5ee9c62434056f5ecf
CaweAlves <contato@cawe.dev>
2024-05-21 15:21:00 -0300

    plp-1: creating menu component

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,17 +1,3 @@
-<div class="grid grid-rows-3 md:grid-rows-1 grid-cols-1 md:grid-cols-3">
-    <div class="text-center md:text-left md:ml-5">
-        <h1>LOGO</h1>
-    </div>
-
-    <div class="text-center md:flex justify-center gap-5">
-        <h1>HOME</h1>
-        <h1>PRODUTOS</h1>
-        <h1>LOCALIZAÇÃO</h1>
-        <h1>CONTATO</h1>
-    </div>
-
-    <div class="text-center mt-5 md:mt-0 md:flex justify-end md:mx-5">
-        <h1>Sou Funcionario</h1>        
-    </div>
-
+<div>
+    <livewire:menu class="fixed"/>
 </div>
